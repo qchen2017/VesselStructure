dff6fedfa4e8288cf102470f37eb383c9b3a1f40
rshruthi91 <rshruthi91@users.noreply.github.com>
2016-04-10 21:57:04 -0400

    Common file included everywhere for any typedefs or variables or constants that need to be shared

---
+++ common.h
@@ -0,0 +1,14 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <QDebug>
+#include <QLinkedList>
+#include <QMap>
+#include <QMutableMapIterator>
+#include <QMapIterator>
+#include <QFile>
+#include <QTextStream>
+#include <QVector>
+#include <inttypes.h>
+
+#endif // COMMON_H
